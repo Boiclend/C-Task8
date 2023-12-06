284b4343dea04415bc4ffca7703a5555c6d6bae6
Roman <user@email.com>
2023-12-06 21:30:59 +0500

    added task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿string Message(string message) {
+    Console.WriteLine(message);
+    string text = Console.ReadLine();
+    return text;
+}
+void GetCountOfSymbols(string txt) {
+    foreach (char symbol in txt.Distinct().ToArray())
+    {
+        int count = txt.Count(chr => chr == symbol);
+        Console.WriteLine($"Количество символов {symbol} = {count}");
+    }
+}
+
+string Text = Message("введите строку");
+GetCountOfSymbols(Text);
+
