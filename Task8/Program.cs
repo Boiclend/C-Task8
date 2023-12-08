83cb7b1b31f642be63c120a48a8a1988dba3a05c
Roman <user@email.com>
2023-12-08 20:00:31 +0500

    added remarks

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,12 +1,17 @@
-﻿string Message(string message) {
+﻿// Подсчитать количество повторений элементов, заданного множества символов,
+// во введенной строке. Суть задачи такова, вводим строку символов,
+// после чего подсчитываем количество повторений каждого символа в строке. 
+// Таким образом количество повторений любого символа будет равно как минимум единице.
+
+string Message(string message) {
     Console.WriteLine(message);
     string text = Console.ReadLine();
     return text;
 }
-void GetCountOfSymbols(string txt) {
-    foreach (char symbol in txt.Distinct().ToArray())
+void GetCountOfSymbols(string txt) { // поиск количества повторений каждого символа в строке
+    foreach (char symbol in txt.Distinct().ToArray()) // Distinct() - удаляет дубликаты 
     {
-        int count = txt.Count(chr => chr == symbol);
+        int count = txt.Count(chr => chr == symbol); // считаем количество повторений символа находящегося в переменной symbol
         Console.WriteLine($"Количество символов {symbol} = {count}");
     }
 }
